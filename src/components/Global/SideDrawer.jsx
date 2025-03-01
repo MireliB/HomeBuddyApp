9dbfcf2ba3419853678938fc2556e101cdeb3950
Mirel <89742157+MireliB@users.noreply.github.com>
2025-03-01 17:01:23 +0200

    changed back to what was before in sidebar

--- src/components/Global/SideDrawer.jsx
+++ src/components/Global/SideDrawer.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useState } from "react";
 import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
 import {
   HomeOutlined as HomeIcon,
@@ -11,7 +11,7 @@ import {
   Menu as MenuIcon,
   Notifications as NotificationsIcon,
   ExitToApp as LogoutIcon,
-  People as ClientIcon,
+  People as ClientIcon
 } from "@mui/icons-material";
 
 import {
@@ -23,76 +23,65 @@ import {
 } from "@mui/material";
 
 import "react-pro-sidebar/dist/css/styles.css";
-import "./SideDrawer.module.css";
-import { useLocation, useNavigate } from "react-router-dom";
+import './SideDrawer.module.css'
+import { useNavigate } from "react-router-dom";
+
+const navigationItems = [
+  { path: "/home", name: "Home", icon: <HomeIcon /> },
+  { path: "/dashboard", name: "Dashboard", icon: <DashboardIcon /> },
+  { path: "/roomsPage", name: "Rooms", icon: <RoomIcon /> },
+  { path: "/clients", name: "Clients", icon: <ClientIcon /> },
+  { path: "/aboutUs", name: "About", icon: <InfoIcon /> },
+  { path: "/contacts", name: "Contact", icon: <HelpIcon /> },
+  { path: "/finances", name: "Finances", icon: <MoneyIcon /> },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: <NotificationsIcon />,
+  },
+  { path: "/settings", name: "Settings", icon: <SettingsIcon /> },
+  { path: "/logout", name: "Logout", icon: <LogoutIcon /> },
+];
 
 export default function SideDrawer({ onLogout, isLoggedIn }) {
-  const navigationItems = useMemo(
-    () => [
-      { path: "/home", name: "Home", icon: <HomeIcon /> },
-      { path: "/dashboard", name: "Dashboard", icon: <DashboardIcon /> },
-      { path: "/roomsPage", name: "Rooms", icon: <RoomIcon /> },
-      { path: "/clients", name: "Clients", icon: <ClientIcon /> },
-      { path: "/aboutUs", name: "About", icon: <InfoIcon /> },
-      { path: "/contacts", name: "Contact", icon: <HelpIcon /> },
-      { path: "/finances", name: "Finances", icon: <MoneyIcon /> },
-      {
-        path: "/notifications",
-        name: "Notifications",
-        icon: <NotificationsIcon />,
-      },
-      { path: "/settings", name: "Settings", icon: <SettingsIcon /> },
-      { path: "/logout", name: "Logout", icon: <LogoutIcon /> },
-    ],
-    []
-  );
-
   const theme = useTheme();
 
   const isDarkMode = theme.palette.mode === "dark";
 
   const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const location = useLocation();
-  const [selected, setSelected] = useState(location.pathname);
+  const [selected, setSelected] = useState("Dashboard");
 
   const nav = useNavigate();
 
-  const collapsedHandler = useCallback(
-    () => setIsCollapsed((prev) => !prev),
-    []
-  );
+  const collapsedHandler = () => setIsCollapsed(!isCollapsed);
 
-  useEffect(() => {
-    setSelected(location.pathname);
-  }, [location.pathname]);
+  const handleMouseEnter = (name) => setSelected(name);
 
-  const handleNavigation = useCallback(
-    (path) => {
-      if (location.pathname === path) return;
-      if (path === "/logout") {
-        onLogout();
-        nav("/login");
-      } else {
-        nav(path);
-      }
-    },
-    [nav, onLogout, location.pathname]
-  );
+  const handleMouseLeave = () => setSelected("");
+
+  const handleLogOut = (path) => {
+    if (path === "/logout") {
+      onLogout();
+      nav("/login");
+    } else {
+      nav(path);
+    }
+  };
 
   return (
     <Box height="100%">
       {isLoggedIn && (
         <Box
-        sx={{
-          height: "100vh",
-          "& .pro-sidebar-inner, & .pro-icon-wrapper": {
-            background: `${isDarkMode ? "#1f2a40" : "#e0e0e0"} !important`,
-          },
-          "& .pro-inner-item:hover, & .pro-inner-item.active": {
-            color: "#868dfb !important",
-          },
-        }}
+          sx={{
+            height: "100vh",
+            "& .pro-sidebar-inner, & .pro-icon-wrapper": {
+              background: `${isDarkMode ? "#1f2a40" : "#e0e0e0"} !important`,
+            },
+            "& .pro-inner-item:hover, & .pro-inner-item.active": {
+              color: "#868dfb !important",
+            },
+            "& .pro-inner-item.active": { color: "#868dfb !important" },
+          }}
         >
           <CssBaseline />
           <ProSidebar collapsed={isCollapsed}>
@@ -106,19 +95,19 @@ export default function SideDrawer({ onLogout, isLoggedIn }) {
                     />
                   ) : undefined
                 }
-                sx={{ margin: "10px 0 20px 0" }}
+                style={{ margin: "10px 0 20px 0" }}
               >
                 {!isCollapsed && (
                   <Box
                     display="flex"
                     justifyContent="space-between"
                     alignItems="center"
-                    ml="15px"
+                    ml="10px"
                   >
                     <Typography
                       variant="h4"
-                      fontWeight="bold"
                       color={isDarkMode ? "#e0e0e0" : "#1f2a40"}
+                      fontWeight="bold"
                     >
                       HOME BUDDY
                     </Typography>
@@ -132,23 +121,23 @@ export default function SideDrawer({ onLogout, isLoggedIn }) {
                 )}
               </MenuItem>
 
-              <Box sx={{ paddingLeft: isCollapsed ? undefined : "10%" }}>
-                {navigationItems.map(({ path, name, icon }, index) => (
-                    <MenuItem
-                      key={index}
-                      icon={icon}
-                      onClick={() => handleNavigation(path)}
-                      active={selected === path}
-                      style={{
-                        color: isDarkMode ? "#e0e0e0" : "#1f2a40",
-                        fontWeight: "bold",
-                        textAlign: "left",
-                      }}
-                    >
-                      {name}
-                    </MenuItem>
-                ))}
-              </Box>
+              {navigationItems.map(({ path, name, icon }, index) => (
+                <MenuItem
+                  key={index}
+                  icon={icon}
+                  onClick={() => handleLogOut(path)}
+                  active={selected === name}
+                  onMouseEnter={() => handleMouseEnter(name)}
+                  onMouseLeave={handleMouseLeave}
+                  style={{
+                    color: isDarkMode ? "#e0e0e0" : "#1f2a40",
+                    fontWeight: "bold",
+                    textAlign: "left",
+                  }}
+                >
+                  {name}
+                </MenuItem>
+              ))}
             </Menu>
           </ProSidebar>
         </Box>
