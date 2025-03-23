b8bb5a27b04a4e2f4a43e7958bf7b910e974ddab
Mirel <89742157+MireliB@users.noreply.github.com>
2025-03-23 19:21:07 +0200

    moved mongoose connection to .env file

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,23 +1,19 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 
-// const jwtSecret = crypto.randomBytes(64).toString("base64");
-
 const initMongo = async () => {
-  const mongoUrl =
-    "mongodb+srv://mireloosh2:XQDNm8EZK39RzoE9@home-buddy.vjsju.mongodb.net/";
-
   try {
-    await mongoose.connect(mongoUrl);
-    console.log("MongoDB connected successfully");
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
-    console.error("Mongoose connection error:", error.message);
+    console.error("❌ Mongoose connection error:", error.message);
     process.exit(1); 
   }
 
   const db = mongoose.connection;
-  db.on("connected", () => console.log("Mongoose connected to the database"));
-  db.on("error", (err) => console.error("Mongoose connection error:", err));
-  db.on("disconnected", () => console.log("Mongoose disconnected"));
+  db.on("connected", () => console.log("🔹 Mongoose connected to the database"));
+  db.on("error", (err) => console.error("❗ Mongoose connection error:", err));
+  db.on("disconnected", () => console.log("⚠️ Mongoose disconnected"));
 };
 
 module.exports = initMongo;
