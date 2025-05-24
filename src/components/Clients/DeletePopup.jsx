1fb100a4a73a3bd3559aa5f1a2eda9f8014ab1c7
Mirel <89742157+MireliB@users.noreply.github.com>
2025-05-24 20:50:43 +0300

    .

---
+++ src/components/Clients/DeletePopup.jsx
@@ -0,0 +1,33 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
+import React from 'react'
+import useClients from '../../Hooks/useClients';
+
+export default function DeletePopup({
+  isPopupOpen,
+  setIsPopupOpen,
+  confirmDeleteUser,
+  selectedUser,
+  setSelectedUser,
+}) {
+  return (
+    <Dialog
+      open={isPopupOpen}
+      onClose={() => {
+        setIsPopupOpen(false);
+      }}
+    >
+      <DialogTitle>Confirm Delete</DialogTitle>
+      <DialogContent>
+        Are you sure you want to delete current client?
+        <DialogActions sx={{ justifyContent: "space-between", mt: 2 }}>
+          <Button sx={{ color: "red" }} onClick={confirmDeleteUser}>
+            Delete
+          </Button>
+          <Button sx={{ color: "white" }} onClick={() => setIsPopupOpen(false)}>
+            Cancel
+          </Button>
+        </DialogActions>
+      </DialogContent>
+    </Dialog>
+  );
+}
